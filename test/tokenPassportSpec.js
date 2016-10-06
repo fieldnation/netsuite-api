403acb8aed56ba0d116fdd818c304a6f85062d97
Evan Duncan <evan.duncan@fieldnation.com>
2016-10-06 15:00:08 -0500

    Add test fixtures and create unit tests for netsuite abstraction

--- test/tokenPassportSpec.js
+++ test/tokenPassportSpec.js
@@ -1,7 +1,10 @@
 const should = require('chai').should();
+const expect = require('chai').expect;
+const libxml = require('libxmljs');
 const TokenPassport = require('../lib/tokenPassport');
 
-describe('TokenPassport Type', () => {
+
+describe('TokenPassport', () => {
   let passport;
 
   beforeEach(() => {
@@ -12,38 +15,62 @@ describe('TokenPassport Type', () => {
     it('should have an account', () => passport.should.have.property('account'));
     it('should have a consumerKey', () => passport.should.have.property('consumerKey'));
     it('should have a token', () => passport.should.have.property('token'));
-    it('should have a nonce', () => passport.should.have.property('nonce'));
-    it('should have a timestamp', () => passport.should.have.property('timestamp'));
     it('should have a signature', () => passport.should.have.property('signature'));
   });
 
-  context('#timestamp', () => {
+  context('#getTimestamp', () => {
     it('should be a valid timestamp', () => {
-      const d = new Date(passport.timestamp).getTime();
+      const d = new Date(TokenPassport.getTimestamp()).getTime();
       d.should.not.be.NaN;
       d.should.be.above(0);
-      d.should.equal(passport.timestamp);
+    });
+
+    it('should generate a new timestamp every time it is invoked', done => {
+      const expected = TokenPassport.getTimestamp();
+
+      setTimeout(() => {
+        const actual = TokenPassport.getTimestamp();
+        expected.should.not.equal(actual);
+        done();
+      }, 25);
     });
   });
 
   context('#nonce', () => {
-    it('should be between 6 and 64 characters', () => {
-      passport.nonce.length.should.be.least(6).and.most(64);
-    });
+    let nonce;
+    beforeEach(() => nonce = TokenPassport.getNonce());
 
-    it('should not contain the word "undefined"', () => {
-      passport.nonce.should.not.contain('undefined');
-    });
+    it('should be between 6 and 64 characters', () => nonce.length.should.be.least(6).and.most(64));
+    it('should not contain the word "undefined"', () => nonce.should.not.contain('undefined'));
   });
 
-  context('get baseString', () => {
+  context('#getBaseString', () => {
     it('should ampersand delimit the NetSuite Account ID, Consumer Key, Token, Nonce, and Timestamp', () => {
       // random values created from md5 hashes
       passport.account = '1234567';
       passport.consumerKey = 'eb72c61b011a80c870ba44cfc5fb5c13';
       passport.token = '4925397c4fde92c3ccefd398948e0758';
-      const expected = `${passport.account}&${passport.consumerKey}&${passport.token}&${passport.nonce}&${passport.timestamp}`;
-      passport.baseString.should.equal(expected);
+      const nonce = TokenPassport.getNonce();
+      const timestamp = TokenPassport.getTimestamp();
+      const expected = `${passport.account}&${passport.consumerKey}&${passport.token}&${nonce}&${timestamp}`;
+      passport.getBaseString(nonce, timestamp).should.equal(expected);
     });
   });
+
+  context('#generateHeaders', () => {
+    it('should be valid XML', () => {
+      try {
+        passport.account = '123';
+        passport.consumerKey = '0987a';
+        passport.token = 'foobar';
+        passport.signature = { algorithm: 'sha1', value: 'abcd=' };
+
+        const headers = passport.generateHeaders('mustbe6chars', Date.now());
+        
+        libxml.parseXml(headers);
+      } catch (err) {
+        expect(err).to.be.null;
+      }
+    });
+  })
 });
